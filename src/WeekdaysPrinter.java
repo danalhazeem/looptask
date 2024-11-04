64a7c9f8db1ea71b8941f56e69e815e49525a9e5
danalhazeem <92695924+danalhazeem@users.noreply.github.com>
2024-11-04 08:10:54 +0300

    Initial commit

---
+++ src/WeekdaysPrinter.java
@@ -0,0 +1,10 @@
+public class WeekdaysPrinter {
+    public static void main(String[] args) {
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        for (int i = 0; i < days.length; i++) {
+            if (i >= 5) break;
+            System.out.println(days[i]);
+        }
+    }
+}
