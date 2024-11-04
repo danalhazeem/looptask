b24cad9a324800702bab6820a7851ac3b4f623f5
danalhazeem <92695924+danalhazeem@users.noreply.github.com>
2024-11-04 09:05:25 +0300

    test

--- src/WeekdaysPrinter.java
+++ src/WeekdaysPrinter.java
@@ -3,7 +3,7 @@ public class WeekdaysPrinter {
         String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
         for (int i = 0; i < days.length; i++) {
-            if (i >= 5) break;
+            if (i >= 3) break;
             System.out.println(days[i]);
         }
     }
